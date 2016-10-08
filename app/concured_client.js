15c60bdeb4d657f01b528fff84771244348f1b9a
jpbeaudet <jpbeaudet84@gmail.com>
2016-10-08 19:41:16 -0400

    added a test call to the api

--- app/concured_client.js
+++ app/concured_client.js
@@ -3,4 +3,18 @@
 	
 	// TODO : Here will go all jquery stuff
 	
+	// Test Concured api connection
+	if ($('#test').length) {
+	$.getJSON( "http://localhost:3000/api", function( data ) {
+	var items = [];
+	$.each( data, function( key, val ) {
+		items.push( "<li id='" + key + "'>" + val + "</li>" );
+	});
+ 
+	$( "<ul/>", {
+		"class": "my-new-list",
+		html: items.join( "" )
+	}).appendTo( "body" );
+	});
+	}
 })(jQuery);
