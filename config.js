1bc88d063699f5408791d7b0fa7b5ae68371b6b7
Jean-Philippe Beaudet <jpbeaudet84@gmail.com>
2016-10-24 13:20:50 -0400

    removed overflow for selected topics

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 var Config = {
     node_web_server_host: "localhost",
-    node_web_server_port: 8080,
+    node_web_server_port: 4000,
     mongodb_database: 'concured_node_dev',
     mongodb_host : "localhost"
 };
